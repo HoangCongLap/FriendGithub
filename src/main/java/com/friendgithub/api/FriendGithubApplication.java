ea5fb5919a97c79deece3046d6d5958240adea06
laphc <lapconghoang@gmail.com>
2025-01-04 13:02:09 +0700

    first commit

---
+++ src/main/java/com/friendgithub/api/FriendGithubApplication.java
@@ -0,0 +1,14 @@
+package com.friendgithub.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendGithubApplication.class, args);
+		System.out.println("test");
+	}
+
+}
---
+++ src/test/java/com/friendgithub/api/FriendGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friendgithub.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
